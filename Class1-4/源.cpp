bcf56f46855564b8928af85772cd64dc5407067c
天昱 郑 <1219979760@qq.com>
2020-02-28 14:51:56 +0800

    网络差传不上去

---
+++ Class1-4/源.cpp
@@ -0,0 +1,40 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	cv::Mat srcMat = imread("D:\\a.jpg");
+	cv::Mat deepMat;
+	cv::Mat shallowMat;
+	shallowMat = srcMat;
+	srcMat.copyTo(deepMat);
+	int height = srcMat.rows;
+	int width = srcMat.cols;
+	uchar threshold = 100;
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++)
+		{
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1] + srcMat.at<Vec3b>(j, i)[2]) / 3;
+			if (average > threshold)
+			{
+				average = 255;
+			}
+			else
+			{
+				average = 0;
+			}
+			srcMat.at<Vec3b>(j, i)[0] = average;
+			srcMat.at<Vec3b>(j, i)[1] = average;
+			srcMat.at<Vec3b>(j, i)[2] = average;
+		}
+	}
+	imshow("deepMat", deepMat);
+	imshow("shallowMat", shallowMat);
+	waitKey(0);
+	return 0;
+
+}
